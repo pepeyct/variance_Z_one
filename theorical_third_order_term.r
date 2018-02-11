271292a2d24349d9cd4ca688cb6d933dbb3718ec
Chong <zhaochongyct@gmail.com>
2018-02-10 22:33:19 -0500

    modified version Feb 10

--- theorical_third_order_term.r
+++ theorical_third_order_term.r
@@ -1,18 +1,19 @@
 # 
 set.seed(123)
 #
+setwd("C:/Users/CZhao/Dropbox/Research/PairCorrelationFunction/simulation/SimulationFinal/inference/theoretical variance/variance_Z_one/variance_Z_one")
 library(dplyr)
 library(data.table)
 library(foreach)
 library(doParallel)
 library(doSNOW)
 ###############################################
-rho = 0.8
-mu = 3.0
+rho   = 1.0
 sigma = 0.030
+mu=6
 R=0.18
 DT=30
-m=200
+m=100
 h.max = 0.016
 h=0.010
 #
@@ -33,7 +34,7 @@ pcf=function(t){
 }
 pcfGrid=pcf(grid)
 ### all combinations of three grid vectors that can be used to appximate the calculas
-nn1 = ceiling((R+h.max)/bin_grid_c)
+nn1 = ceiling((R+h+0.002)/bin_grid_c)
 grid_list =list(n_grid_c)
 for( i in 1:n_grid_c){
   point_u = grid_c[i]
@@ -49,35 +50,31 @@ for( i in 1:n_grid_c){
 }
 ###################################################
 grid_list_frame = data.frame(matrix(unlist(grid_list),byrow = T,ncol = 3))
-
-grid_triple_point = grid_list_frame %>% mutate(dis1 = abs(X1-X2),dis2 = abs(X1-X3),dis3 = abs(X2-X3))
-# grid_uv=expand.grid(grid_c,grid_c)
-# grid_uv = grid_uv %>% mutate(dis=abs(Var1-Var2))
-# grid_uv_mat =grid_uv[grid_uv$dis <=R+h.max,]
-# grid_double_uv = expand.grid(grid_uv$Var1,grid_c)
-
+grid_triple_point_fill = grid_list_frame %>% mutate(dis1 = abs(X1-X2),dis2 = abs(X1-X3),dis3 = abs(X2-X3))
+rm(grid_list_frame,grid_list)
+gc()
+# calculate the calculus 
+normal_fun = function(x){exp(-x^2/(2*sigma^2))/(sigma*sqrt(2*pi))}
 #
-cl = makeCluster(4, type = "SOCK")
+cl = makeCluster(2, type = "SOCK")
 registerDoSNOW(cl)
 
 ptm<-proc.time()
-Q_mat_cal = foreach(i = 1:50,.combine = 'rbind',.packages = c("dplyr")) %dopar% {
+calculus_cal = foreach(i = 1:50,.combine = 'rbind',.packages = c("dplyr")) %dopar% {
   r=grid[i] # each time point that pcf being estimated 
-  grid_uv =grid_uv_mat %>% mutate(w_h = ifelse(abs(dis-r)<h,1/(2*h*(DT-dis)),0))
-  grid_uv = grid_uv[grid_uv$w_h>0,]
-  grid_uv = grid_uv %>% mutate(lam_prod = lam_fun(Var1)*lam_fun(Var1))
-  grid_uv = grid_uv %>% mutate(q1=lam_prod*w_h,q2 = (DT*h)*lam_prod*w_h^2)
-  Q1 =calculus_bin_area*sum(grid_uv$q1)
-  Q2 =calculus_bin_area*sum(grid_uv$q2)
-  c(Q1,Q2)
+  grid_triple_point =grid_triple_point_fill %>% mutate(w_h_1 = ifelse(abs(dis1-r)<h,1/(2*h*(DT-dis1)),0),
+                                                  w_h_2= ifelse(abs(dis2-r)<h,1/(2*h*(DT-dis2)),0))
+  grid_triple_point = grid_triple_point[grid_triple_point$w_h_1>0 & grid_triple_point$w_h_2>0,]
+  # calculate the third order pair correlation fun
+  g3=apply(grid_triple_point,1,function(row_Vec){integrate(f=function(p){normal_fun(p+row_Vec[1])*normal_fun(p+row_Vec[2])*normal_fun(p+row_Vec[3])},lower=0,upper=R)$value})
+  grid_triple_point = grid_triple_point %>% mutate(g3_cls = g3+pcf(dis1)+pcf(dis2)+pcf(dis3)-2,a12=(dis2-r)/h,a21=(dis1-r)/h,a22=((dis2-r)*(dis1-r))/(h^2))
+  grid_triple_point = grid_triple_point %>% mutate(c11=w_h_1*w_h_2*g3_cls,c12 = a12*c11,c21 = a21*c11,c22=a22*c11)
+  C_vec= colSums(grid_triple_point[,13:16])*bin_grid_c^3
+  C_vec
 }
-
-
-
-
-
-
-
+save(calculus_cal,file = "calculus_cal.RData")
+proc.time()-ptm
+# three order calculus
 
 
 
